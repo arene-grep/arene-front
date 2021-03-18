bb4323e8e8edcaae2b3f408e09d2e127c076bbe7
Shyrzy <pedrothebossur@hotmail.com>
2021-03-18 13:27:13 +0100

    Update des commandes

--- src/components/Orders/ListOrders.vue
+++ src/components/Orders/ListOrders.vue
@@ -1,13 +1,39 @@
 <template>
   <div>
+            <div class="md-layout">
+              <div class="md-layout-item">
+      </div>
+      <div class="md-layout-item">
+        <md-field md-clearable>
+          <label>Numéro commande</label>
+          <md-input v-model="order_number" placeholder="Numéro de commande" v-on:keydown.enter="searchOrder"></md-input>
+        </md-field>
+      </div>
+      <div class="md-layout-item">
+      </div>
+      <div class="md-layout-item">
+        <md-field md-clearable>
+          <label>Email client</label>
+          <md-input v-model="user" placeholder="Email du client" v-on:keydown.enter="searchOrder"></md-input>
+        </md-field>
+      </div>
+      <div class="md-layout-item-button">
+        <md-button class="md-primary" @click="searchOrder">Chercher</md-button>
+      </div>
+      <div class="md-layout-item">
+      </div>
+    </div>
     <md-list class="md-triple-line">
       <md-card-header>
         <div class="md-title">Vos commandes</div>
       </md-card-header>
     </md-list>
     <md-list class="md-triple-line">
+      <div>
+        <md-content class="md-primary">
+      <label>Commandes ouvertes</label>
       <div v-for="order in orders" :key="order.id">
-        <md-list-item :to="{name: 'getOrder', params:{id:order.id}}">
+        <md-list-item v-if="checkStatus(order.status)" :to="{name: 'getOrder', params:{id:order.id}}">
           <md-avatar >
             <img src="../../assets/logo-arene.png">
           </md-avatar>
@@ -17,6 +43,25 @@
           </div>
         </md-list-item>
         <md-divider class="md-inset"></md-divider>
+      </div>
+      </md-content>
+            </div>
+      <div>
+        <md-content class="md-accent">
+      <label>Commandes fermées</label>
+      <div v-for="order in orders" :key="order.id">
+        <md-list-item v-if="checkStatus(order.status)==false" :to="{name: 'getOrder', params:{id:order.id}}">
+          <md-avatar >
+            <img src="../../assets/logo-arene.png">
+          </md-avatar>
+          <div class="md-list-item-text">
+            <span>Numéro de commande : {{ order.id }}</span>
+            <span>Date : {{ order.date }}</span>
+          </div>
+        </md-list-item>
+        <md-divider class="md-inset"></md-divider>
+      </div>
+      </md-content>
       </div>
     </md-list>
   </div>
@@ -37,8 +82,18 @@ export default {
           this.orders = data
         })
         .catch(err => console.log(err))
-  }
+  },
+    methods: {
+    checkStatus (status) {
+      if (status == 'commande terminée') { return false } else { return true }
+    },
+
+        searchOrder () {
+      return true
+    },
+    }
 }
+
 
 </script>
 
@@ -49,5 +104,10 @@ export default {
   display: inline-block;
   vertical-align: top;
   border: 1px solid rgba(#000, .12);
+}
+
+.md-layout {
+  margin-left: 20px;
+  margin-right: 20px;
 }
 </style>
